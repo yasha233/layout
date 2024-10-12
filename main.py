9abc3529a9d3eeb1b07ea83291794fcffd889fd4
Daniil <skuratov.danya@gmail.com>
2024-10-12 16:48:16 +0300

    sys of load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 
+
 class TextAnalyzer:
     def __init__(self, filename, symbols):
         self.filename = filename
@@ -33,11 +34,22 @@ class TextAnalyzer:
 
 def main():
     filename = 'C:/voina-i-mir.txt'  # Имя файла с текстом
-    top_row = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13) # должно быть 5
-    symbol_counter = TextAnalyzer(filename, top_row)
+    rows = (
+        (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13),
+        (16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27),
+        (30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40),
+        (42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53),
+        (57,)
+    )
+    rows_symbols = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',  # Цифровой ряд
+                    'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ',  # Верхний ряд
+                    'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э',  # Средний ряд
+                    'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', ',',  # Нижний ряд
+                    ' '  # Дополнительный
+                    )
+    symbol_counter = TextAnalyzer(filename, rows_symbols)
     symbol_counter.count_symbols()
     symbol_counter.display_counts()
-
 
 
 if __name__ == "__main__":
