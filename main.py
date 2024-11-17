d38ce02682976f658eb9eae4fca28130e61e116e
Daniil <skuratov.danya@gmail.com>
2024-11-17 16:54:02 +0300

    добавить тесты

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from visualization import draw_histogram
 
 
 def main():
-    filename = (r'C:\voina-i-mir.txt', r'C:\1grams-3.txt')  # Имя файла с текстом
+    filename = (r'C:\1.txt', r'C:\1.txt')  # Имя файла с текстом
     symb = (('!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', '/', ','),
             ('№', '%', ':', ';', '-', '"', '(', ')', '+', 'ъ', '?', '!', '_'))
 
@@ -27,9 +27,8 @@ def main():
         symbol_counter = TextAnalyzer(filename_i, keylout_dd, symb)
         final_loads = symbol_counter.count_symbols()
         # symbol_counter.display_counts()
-        draw_histogram('йцукен', final_loads[0])
+        draw_histogram(filename_i, final_loads)
         print(final_loads)
-        draw_histogram('diktor', final_loads[1])
 
 
 if __name__ == "__main__":
--- visualization.py
+++ visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 
 
 def draw_histogram(name, data):
@@ -13,13 +14,33 @@ def draw_histogram(name, data):
     Возвращает:
     None
     """
-    labels = list(data.keys())
-    values = list(data.values())
-
+    dict1 = data[0]
+    dict2 = data[1]
+    keys = list(dict1.keys())
+    values1 = list(dict1.values())
+    values2 = list(dict2.values())
+    width = 0.35  # Ширина баров
+    x = range(len(keys))  # Позиции по оси X
+    file_size = os.path.getsize(name)
+    file_name = os.path.basename(name)
     # создание гистограммы
-    plt.bar(labels, values)
+    fig, ax = plt.subplots()
 
-    plt.title(name)
-    plt.xlabel('Пальцы')
-    plt.ylabel('Количество нажатий')
+    # Рисуем бары для первого словаря
+    rects1 = ax.barh([pos - width / 2 for pos in x], values1, width, label='йцукен', color='black')
+
+    # Рисуем бары для второго словаря
+    rects2 = ax.barh([pos + width / 2 for pos in x], values2, width, label='diktor', color='orange')
+    # Настройка осей
+    for i in range(len(keys)):
+        if values1[i] != 0:
+            xpos = values1[i]
+        else:
+            xpos = values2[i]
+        ax.text(xpos + 0.1, x[i] - width / 2 - 0.1, str(abs(values1[i] - values2[i])), ha='center', va='bottom')
+    ax.set_yticks(x)
+    ax.set_yticklabels(keys)
+    ax.set_xlabel('Количество нажатий')
+    ax.set_title(str(file_name) + '    ' + str(file_size) + ' MB')
+    ax.legend()
     plt.show()
