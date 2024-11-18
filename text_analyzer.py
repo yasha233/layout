df0f70c6abc516c8ccf2ecc8ab6b1f9630e9bb19
Daniil <skuratov.danya@gmail.com>
2024-11-18 19:21:26 +0300

    добавлена 3 раскладка

--- text_analyzer.py
+++ text_analyzer.py
@@ -12,6 +12,7 @@ class TextAnalyzer:
         self.shifts = shifts
         self.finger_load = {finger: 0 for finger in symbols.keys()}
         self.finger_load2 = {finger2: 0 for finger2 in symbols.keys()}
+        self.finger_load3 = {finger3: 0 for finger3 in symbols.keys()}
 
     def find_finger(self, char):
         """
@@ -20,10 +21,13 @@ class TextAnalyzer:
         :param char: Символ, для которого нужно определить палец.
         :return: Список, содержащий два элемента: палец в каждой раскладке.
         """
-        i = [None, None]
+        i = [None, None, None]
+        # print(self.symbols.items())
         for finger, layouts in self.symbols.items():
             for layout in layouts:
                 if char in layout:
+                    if layouts.index(layout) == 2:
+                        i[2] = finger
                     if layouts.index(layout) == 1:
                         i[1] = finger
                     if layouts.index(layout) == 0:
@@ -44,6 +48,8 @@ class TextAnalyzer:
                         t = self.find_finger(char.lower())
                         finger = t[0]
                         finger2 = t[1]
+                        finger3 = t[2]
+
                         for symb_with_shift in self.shifts[0]:
                             if char == symb_with_shift and finger != "lfi5m":
                                 if "lfi5м" in self.finger_load:
@@ -51,6 +57,7 @@ class TextAnalyzer:
                                 else:
                                     if "rfi5м" in self.finger_load:
                                         self.finger_load["rfi5м"] += 1
+
                         for symb_with_shift in self.shifts[1]:
                             if char == symb_with_shift and finger2 != "lfi5m":
                                 if "lfi5м" in self.finger_load2:
@@ -58,13 +65,24 @@ class TextAnalyzer:
                             if char == symb_with_shift and finger2 == "lfi5m":
                                 if "rfi5м" in self.finger_load2:
                                     self.finger_load2["rfi5м"] += 1
+
+                        for symb_with_shift in self.shifts[2]:
+                            if char == symb_with_shift and finger3 != "lfi5m":
+                                if "lfi5м" in self.finger_load3:
+                                    self.finger_load3["lfi5м"] += 1
+                            if char == symb_with_shift and finger3 == "lfi5m":
+                                if "rfi5м" in self.finger_load3:
+                                    self.finger_load3["rfi5м"] += 1
+
                         if char.isupper():
+
                             if finger != "lfi5м":
                                 if "lfi5м" in self.finger_load:
                                     self.finger_load["lfi5м"] += 1
                             else:
                                 if "rfi5м" in self.finger_load:
                                     self.finger_load["rfi5м"] += 1
+
                             if finger2 != "lfi5м":
                                 if "lfi5м" in self.finger_load2:
                                     self.finger_load2["lfi5м"] += 1
@@ -72,26 +90,37 @@ class TextAnalyzer:
                                 if "rfi5м" in self.finger_load2:
                                     self.finger_load2["rfi5м"] += 1
 
+                            if finger3 != "lfi5м":
+                                if "lfi5м" in self.finger_load3:
+                                    self.finger_load3["lfi5м"] += 1
+                            else:
+                                if "rfi5м" in self.finger_load3:
+                                    self.finger_load3["rfi5м"] += 1
+
                         # Обновляем пальцы, если они существуют в словарях
                         if finger in self.finger_load:
                             self.finger_load[finger] += 1
                         if finger2 in self.finger_load2:
                             self.finger_load2[finger2] += 1
+                            if finger3 in self.finger_load3:
+                                self.finger_load3[finger3] += 1
 
                 if i > 0:
                     if "rfi5м" in self.finger_load:
                         self.finger_load["rfi5м"] += i
                     if "rfi5м" in self.finger_load2:
                         self.finger_load2["rfi5м"] += i
+                    if "rfi5м" in self.finger_load3:
+                        self.finger_load3["rfi5м"] += i
 
-            return self.finger_load, self.finger_load2
+            return self.finger_load, self.finger_load2, self.finger_load3
 
         except FileNotFoundError:
             print("Файл не найден.")
         except IOError:
             print("Ошибка ввода-вывода при работе с файлом.")
-        except Exception as e:
-            print(f"Произошла ошибка: {e}")
+        # except Exception as e:
+        #     print(f"Произошла ошибка: {e}")
 
     def display_counts(self):
         """
@@ -103,4 +132,7 @@ class TextAnalyzer:
             print(f"'{symbol}': {count}")
         print('---------------------')
         for symbol, count in self.finger_load2.items():
+            print(f"'{symbol}': {count}")
+        print('---------------------')
+        for symbol, count in self.finger_load3.items():
             print(f"'{symbol}': {count}")
--- visualization.py
+++ visualization.py
@@ -16,34 +16,43 @@ def draw_histogram(path, data):
     """
     dict1 = data[0]
     dict2 = data[1]
+    dict3 = data[2]
     keys = list(dict1.keys())
     values1 = list(dict1.values())
     values2 = list(dict2.values())
+    values3 = list(dict3.values())
     width = 0.2  # Ширина баров
     x = range(len(keys))  # Позиции по оси X
-    file_size = os.path.getsize(path)
+    file_size = round(os.path.getsize(path) / (1024*1024), 2)
     file_name = os.path.basename(path)
     # создание гистограммы
     fig, ax = plt.subplots(figsize=(16, 8))
 
     # Рисуем бары для первого словаря
-    rects1 = ax.barh([pos + width / 2 for pos in x], values1, width, label='йцукен', color='red')
-
-    # Рисуем бары для второго словаря
-    rects2 = ax.barh([pos - width / 2 for pos in x], values2, width, label='diktor', color='green')
-    # Настройка осей
+    ax.barh([pos + width for pos in x], values1, width, label='йцукен', color='red')
+    ax.barh([pos for pos in x], values2, width, label='diktor', color='blue')
+    ax.barh([pos - width for pos in x], values3, width, label='zubachew', color='grey')
     for i in range(len(keys)):
-        if values1[i] != 0:
-            xpos = values1[i]
-        else:
-            xpos = values2[i]
-        ax.text(xpos + 0.1, x[i] - width / 2, str(abs(values1[i] - values2[i])), ha='center', va='bottom')
-        # ax.bar_label(rects1, str(abs(values1[i] - values2[i])))
+        if values1[i] + values2[i] > 0:  # Избегаем деления на ноль
+            diff1 = abs(values2[i] - values1[i]) / values1[i] * 100
+            if values1[i] > values2[i]:
+                xpos = values1[i]
+            else:
+                xpos = values2[i]
+            ax.text(xpos + 0.1, i, f'{diff1:.1f}%', ha='center', va='center', color='black')
+
+        if values2[i] + values3[i] > 0:  # Избегаем деления на ноль
+            diff2 = abs(values3[i] - values2[i]) / values2[i] * 100
+            if values2[i] > values3[i]:
+                xpos = values2[i]
+            else:
+                xpos = values3[i]
+            ax.text(xpos + 0.1, i - 0.4, f'{diff2:.1f}%', ha='center', va='center', color='black')
     ax.set_yticks(x)
     ax.set_yticklabels(keys)
     ax.set_ylabel('Пальцы')
     ax.set_xlabel('Количество нажатий')
-    ax.set_title(str(file_name) + '    ' + str(file_size) + ' MB')
+    ax.set_title(f'{file_name}    {file_size:.2f} MB')
     ax.legend()
 
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
